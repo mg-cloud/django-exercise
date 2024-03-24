6f8b17d4d447035eea58c04626e8af18be5634f6
Maxime Galloux <maxime.galloux@pm.me>
2024-03-24 20:26:55 +0100

    Make sale objects available in admin panel

--- sales/admin.py
+++ sales/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Article, ArticleCategory, Sale
 
-# Register your models here.
+admin.site.register(ArticleCategory)
+admin.site.register(Article)
+admin.site.register(Sale)
